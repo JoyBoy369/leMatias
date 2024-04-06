bd376c59bb426a9f2d2077b30ffb0378233fcd00
JoyBoy369 <acholucas136@gmail.com>
2024-04-06 00:19:54 -0300

    sfsfdfd

--- papulandia/src/papulandia/Main.java
+++ papulandia/src/papulandia/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		System.out.println("Tdaros jungla el matiasos");
+		System.out.println("Tdaros jungsdsadsdsadasdasdasdla el matiasos");
 		
 	}
 
